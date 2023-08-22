83fbd65969e2bc6391f2178df7640c6187514dd7
Ashar <103361807+AsharAli999@users.noreply.github.com>
2023-08-22 22:48:55 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import {BrowserRouter as Router,Routes, Route} from "react-router-dom"
+import {Provider} from "react-redux";
+import store from "./store";
+import './App.css';
+import Nav from "./components/Nav"
+import Home from "./components/Home"
+import Cart from "./components/Cart"
+import Details from "./components/Details";
+
+function App() {
+  return (
+    <Router>
+     <Provider store={store}>
+     <Nav />
+     <Routes>
+
+     <Route path="/" exact element={<Home/>} />
+     <Route path="/cart" exact element={<Cart/>} />
+     <Route path="/details/:id" exact element={<Details/>} />
+     </Routes>
+     </Provider>
+    </Router>
+  );
+}
+
+export default App;
